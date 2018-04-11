c4e985f746836c857f9d709d1e1a9f5b41bacaba
George Raymond <geomarray@gmail.com>
2018-04-10 23:28:31 -0700

    moved the javascript to the game.js file and linked the file using a relative path

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var map;
+var startMapCenter = new google.maps.LatLng(34.1011049,-118.3534514);
+function initialize() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: startMapCenter,
+        zoom: 13
+    });
+    
+    var request = {
+        location: startMapCenter,
+        radius: '5000',
+        type: ['resturaunts']
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+}
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            createMarker(results[i]);
+            console.log(results[i])
+        }
+    }
+}
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+}
+;
+
+google.maps.event.addDomListener(window, 'load', initialize);
