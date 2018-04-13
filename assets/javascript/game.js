319fc341201634ef5a948ab821eab6338d7d2fab
George Raymond <geomarray@gmail.com>
2018-04-12 21:04:52 -0700

    added user imput forms and got the query to run and render pin on the map based on the query

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,50 @@
 var map;
+var locationInput = "";
+var typeInput = "";
+
+function runQuery() {
+    locationInput = $("#placeask").val().trim();
+    typeInput = $("#typeask").val().trim();
+    console.log(typeInput);
+    console.log(locationInput);
+};
+
+$("#userInputButton").on("click", function() {
+    event.preventDefault();
+    runQuery();
+  
+    var request = {
+        location: startMapCenter,
+        radius: '5000',
+        type: [ typeInput ]
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                createMarker(results[i]);
+                console.log(results[i])
+            }
+        }
+    }
+    
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+    
+    }
+    ;
+
+
+});
+
 var startMapCenter = new google.maps.LatLng(34.1011049,-118.3534514);
 function initialize() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -6,33 +52,33 @@ function initialize() {
         zoom: 13
     });
 
-    var request = {
-        location: startMapCenter,
-        radius: '5000',
-        type: ['restaurants']
-    };
+    // var request = {
+    //     location: startMapCenter,
+    //     radius: '5000',
+    //     type: [ typeInput ]
+    // };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+    // service = new google.maps.places.PlacesService(map);
+    // service.nearbySearch(request, callback);
 }
-function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            createMarker(results[i]);
-            console.log(results[i])
-        }
-    }
-}
+// function callback(results, status) {
+//     if (status == google.maps.places.PlacesServiceStatus.OK) {
+//         for (var i = 0; i < results.length; i++) {
+//             var place = results[i];
+//             createMarker(results[i]);
+//             console.log(results[i])
+//         }
+//     }
+// }
 
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+// function createMarker(place) {
+//     var placeLoc = place.geometry.location;
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//     });
 
-}
-;
+// }
+// ;
 
 google.maps.event.addDomListener(window, 'load', initialize);
