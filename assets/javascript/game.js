7833831eef51257be7f5752f9ce12b34d7c1fed2
George Raymond <geomarray@gmail.com>
2018-04-16 11:40:30 -0700

    Small change to the html before I pull the new change that alex pushed up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -53,34 +53,7 @@ function initialize() {
         center: startMapCenter,
         zoom: 13
     });
-
-    // var request = {
-    //     location: startMapCenter,
-    //     radius: '5000',
-    //     type: [ typeInput ]
-    // };
-
-    // service = new google.maps.places.PlacesService(map);
-    // service.nearbySearch(request, callback);
 }
-// function callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//         for (var i = 0; i < results.length; i++) {
-//             var place = results[i];
-//             createMarker(results[i]);
-//             console.log(results[i])
-//         }
-//     }
-// }
 
-// function createMarker(place) {
-//     var placeLoc = place.geometry.location;
-//     var marker = new google.maps.Marker({
-//         map: map,
-//         position: place.geometry.location
-//     });
-
-// }
-// ;
 
 google.maps.event.addDomListener(window, 'load', initialize);
